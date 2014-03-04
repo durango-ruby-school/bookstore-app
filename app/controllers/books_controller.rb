fc63d427fd47e285562faea2211aed4ae7a2e58e
Aaron Renner <aaron@animascodelabs.com>
2014-03-04 02:43:42 +0000

    Book resoure route and path helpers

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -14,7 +14,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new params[:book]
     @book.save
-    redirect_to "/books/#{@book.id}"
+    redirect_to book_path(@book)
   end
   
   def edit
@@ -24,12 +24,12 @@ class BooksController < ApplicationController
   def update
     @book = Book.find params[:id]
     @book.update_attributes params[:book]
-    redirect_to "/books/#{@book.id}"
+    redirect_to book_path(@book)
   end
   
   def destroy
     @book = Book.find params[:id]
     @book.destroy
-    redirect_to "/books"
+    redirect_to books_path
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,3 @@
-Bookstore::Application.routes.draw do
-  get '/books' => 'books#index'
-  get '/books/new' => 'books#new'
-  get '/books/:id' => 'books#show'
-  get '/books/:id/edit' => 'books#edit'
-  post '/books' => 'books#create'
-  put '/books/:id' => 'books#update'
-  delete '/books/:id' => 'books#destroy'
+Bookstore::Application.routes.draw do  
+  resources :books
 end

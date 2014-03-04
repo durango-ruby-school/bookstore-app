a565a14c5edf4c71020ffc0ecb5d899243ed8611
Aaron Renner <aaron@animascodelabs.com>
2014-03-04 02:06:48 +0000

    Add books form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -6,4 +6,14 @@ class BooksController < ApplicationController
   def show
     @book = Book.find params[:id]
   end
+  
+  def new
+    @book = Book.new
+  end
+  
+  def create
+    @book = Book.new params[:book]
+    @book.save
+    redirect_to "/books/#{@book.id}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Bookstore::Application.routes.draw do
   get '/books' => 'books#index'
+  get '/books/new' => 'books#new'
   get '/books/:id' => 'books#show'
+  post '/books' => 'books#create'
 end

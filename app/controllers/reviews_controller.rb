699e42775832c3ed93ad2bfc4d6150089e5de206
Aaron Renner <aaron@animascodelabs.com>
2014-03-17 23:56:13 +0000

    Extract book loading to before filter

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,18 +1,23 @@
 class ReviewsController < ApplicationController
+  before_filter :find_book 
+  
   def show
-    @book = Book.find params[:book_id]
     @review = @book.reviews.find params[:id]
   end
   
   def new
-    @book = Book.find params[:book_id]
     @review = @book.reviews.new
   end
   
   def create
-    @book = Book.find params[:book_id]
     @review = @book.reviews.new params[:review]
     @review.save
     redirect_to [@book, @review]
+  end
+  
+  private
+  
+  def find_book
+    @book = Book.find params[:book_id]
   end
 end

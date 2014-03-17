ca2bf0631fa8484a2ad635b18948d9a342e83d43
Aaron Renner <aaron@animascodelabs.com>
2014-03-17 23:51:42 +0000

    New review functionality

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,17 @@ class ReviewsController < ApplicationController
   def show
     @book = Book.find params[:book_id]
     @review = @book.reviews.find params[:id]
-    #@review = Review.find params[:id]
+  end
+  
+  def new
+    @book = Book.find params[:book_id]
+    @review = @book.reviews.new
+  end
+  
+  def create
+    @book = Book.find params[:book_id]
+    @review = @book.reviews.new params[:review]
+    @review.save
+    redirect_to [@book, @review]
   end
 end

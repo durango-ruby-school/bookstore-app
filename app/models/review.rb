3dad5f301f2465966e832491deb12fc2dcb968ad
Aaron Renner <aaron@animascodelabs.com>
2014-03-18 01:29:52 +0000

    Rating is now a dropdown

--- app/models/review.rb
+++ app/models/review.rb
@@ -3,4 +3,5 @@ class Review < ActiveRecord::Base
   attr_accessible :body, :name, :rating
   
   validates_presence_of :name, :body, :rating
+  validates_numericality_of :rating, only_integer: true, greater_than_or_equal_to: 1, less_than_or_equal_to: 5
 end
